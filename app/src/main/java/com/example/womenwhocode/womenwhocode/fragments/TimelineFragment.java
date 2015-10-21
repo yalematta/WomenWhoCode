3cbc6055bc4006247f9f13cca6f777bb5c7b82e4
Shehba Shahab <shehbashahab@gmail.com>
2015-10-20 21:42:19 -0700

    Timeline Fragment and Adapter

--- app/src/main/java/com/example/womenwhocode/womenwhocode/fragments/TimelineFragment.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/fragments/TimelineFragment.java
@@ -2,14 +2,18 @@ package com.example.womenwhocode.womenwhocode.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.womenwhocode.womenwhocode.R;
-import com.example.womenwhocode.womenwhocode.adapters.PostsAdapter;
+import com.example.womenwhocode.womenwhocode.adapters.TimelineAdapter;
 import com.example.womenwhocode.womenwhocode.models.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +22,7 @@ import java.util.List;
  * Created by shehba.shahab on 10/16/15.
  */
 public class TimelineFragment extends Fragment {
-    PostsAdapter aPosts;
+    TimelineAdapter aPosts;
     ArrayList<Post> posts;
     ListView lvPosts;
 
@@ -26,11 +30,10 @@ public class TimelineFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         posts = new ArrayList<>();
-        aPosts = new PostsAdapter(getActivity(), posts);
+        aPosts = new TimelineAdapter(getActivity(), posts);
         populatePostsList();
     }
 
-    // Inflate the fragment layout we defined above for this fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timeline, container, false);
@@ -44,6 +47,18 @@ public class TimelineFragment extends Fragment {
     }
 
     void populatePostsList() {
-        aPosts.clear();
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.orderByAscending("title");
+        query.findInBackground(new FindCallback<Post>() {
+            public void done(List<Post> lvPosts, ParseException e) {
+                if (e == null) {
+                    aPosts.clear();
+                    addAll(lvPosts);
+                    aPosts.notifyDataSetChanged();
+                } else {
+                    Log.d("Message", "Error: " + e.getMessage());
+                }
+            }
+        });
     }
 }
