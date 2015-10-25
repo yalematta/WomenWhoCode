a1d6e62c8d3b0a75449c75a6a0a728121fc28870
zassmin <zmontesd@gmail.com>
2015-10-25 15:43:51 -0700

    get a list of events based on the user's location data

--- app/src/main/java/com/example/womenwhocode/womenwhocode/fragments/EventsFragment.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/fragments/EventsFragment.java
@@ -1,6 +1,10 @@
 package com.example.womenwhocode.womenwhocode.fragments;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,14 +19,21 @@ import android.widget.Toast;
 import com.example.womenwhocode.womenwhocode.R;
 import com.example.womenwhocode.womenwhocode.adapters.EventsAdapter;
 import com.example.womenwhocode.womenwhocode.models.Event;
+import com.example.womenwhocode.womenwhocode.models.Network;
+import com.example.womenwhocode.womenwhocode.models.Profile;
 import com.example.womenwhocode.womenwhocode.utils.LocalDataStore;
 import com.example.womenwhocode.womenwhocode.utils.NetworkConnectivityReceiver;
 import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by shehba.shahab on 10/16/15.
@@ -33,6 +44,10 @@ public class EventsFragment extends Fragment {
     ListView lvEvents;
     private OnEventItemClickListener listener;
     ProgressBar pb;
+    ParseQuery<Profile> profileParseQuery;
+    ParseQuery<Event> query;
+    ParseQuery<Network> networkParseQuery;
+    private static int MILE_RANGE = 25;
 
     public interface OnEventItemClickListener {
         public void onEventClickListener(Event event);
@@ -83,39 +98,49 @@ public class EventsFragment extends Fragment {
     }
 
     private void populateEvents() {
-        /*
-        * TODO: will need to know the user's location
-        * with something like below:
-        * ParseGeoPoint parseGeoPoint = new ParseGeoPoint(37.7016537, -121.9014677);
-        * ParseQuery<Network> networkParseQuery = ParseQuery.getQuery(Network.class);
-        * networkParseQuery.whereWithinMiles(Network.LOCATION_KEY, parseGeoPoint, 25);
-        */
-
-        // TODO: sort events by specific groups
-        // String[] groupIds = new String[]{"11940602"};
-
-        ParseQuery<Event> query = ParseQuery.getQuery(Event.class);
+        // load queries
+        profileParseQuery = ParseQuery.getQuery(Profile.class); // maybe put this is shared prefs!
+        query = ParseQuery.getQuery(Event.class);
+        networkParseQuery = ParseQuery.getQuery(Network.class);
 
         if (!NetworkConnectivityReceiver.isNetworkAvailable(getContext())) {
             query.fromPin(LocalDataStore.EVENT_PIN);
+            networkParseQuery.fromPin(LocalDataStore.LOCAL_NETWORK_PIN); // what if location has changed?
+            profileParseQuery.fromPin(LocalDataStore.PROFILE_PIN);
         }
 
-        query.findInBackground(new FindCallback<Event>() {
-            public void done(List<Event> eventList, ParseException e) {
-                if (eventList == null) {
-                    Toast.makeText(getContext(), "nothing is stored locally", Toast.LENGTH_LONG).show();
-                } else if (e == null) {
-                    aEvents.clear();
-                    aEvents.addAll(eventList);
-                    // hide progress bar, make list view appear
-                    pb.setVisibility(ProgressBar.GONE);
-                    lvEvents.setVisibility(ListView.VISIBLE);
-                    LocalDataStore.unpinAndRepin(eventList, LocalDataStore.EVENT_PIN);
-                } else {
-                    Log.d("PARSE_EVENTS_FAIL", "Error: " + e.getMessage());
+        // TODO: sort events by most recent
+        // TODO: reduce # of network calls for profile
+        // TODO: only update location if it's changed approximately
+
+        profileParseQuery.whereEqualTo(Profile.USER_KEY, ParseUser.getCurrentUser());
+        profileParseQuery.getFirstInBackground(new GetCallback<Profile>() {
+
+            @Override
+            public void done(Profile profile, ParseException e) {
+                if (profile != null) {
+                    networkParseQuery.whereWithinMiles(Network.LOCATION_KEY, profile.getLocation(), MILE_RANGE);
+                    query.whereMatchesQuery(Event.NETWORK_KEY, networkParseQuery);
+                    query.findInBackground(new FindCallback<Event>() {
+                        public void done(List<Event> eventList, ParseException e) {
+                            if (eventList != null && e == null) {
+                                aEvents.clear();
+                                aEvents.addAll(eventList);
+                                // hide progress bar, make list view appear
+                                pb.setVisibility(ProgressBar.GONE);
+                                lvEvents.setVisibility(ListView.VISIBLE);
+                                LocalDataStore.unpinAndRepin(eventList, LocalDataStore.EVENT_PIN);
+                            } else if (e != null) {
+                                Log.d("PARSE_EVENTS_FAIL", "Error: " + e.getMessage());
+                            } else {
+                                Toast.makeText(getContext(), "nothing is stored locally", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
                 }
             }
         });
-    }
 
+
+    }
 }
