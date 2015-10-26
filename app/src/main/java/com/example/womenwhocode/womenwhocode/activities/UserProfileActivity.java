e6d2155c066325d8fd216d889b2a73e6abcaff8c
Puja Roy <pujaroy87@gmail.com>
2015-10-25 23:36:41 -0700

    Implemented photo and camera features

--- app/src/main/java/com/example/womenwhocode/womenwhocode/activities/UserProfileActivity.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/activities/UserProfileActivity.java
@@ -2,9 +2,13 @@ package com.example.womenwhocode.womenwhocode.activities;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,11 +24,15 @@ import com.example.womenwhocode.womenwhocode.models.Network;
 import com.example.womenwhocode.womenwhocode.models.Profile;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,17 +47,20 @@ public class UserProfileActivity extends AppCompatActivity {
     String name="";
     String email="";
     String password="";
-    Profile userProfile;
-   String filepath;
+    Profile userProfile= new Profile();;
+    String filepath;
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
     ArrayAdapter<String> adapterForSpinner;
-private static final int SELECTED_PICTURE=1;
+    private static final int SELECTED_PICTURE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
         Bundle extras = getIntent().getExtras();
-final ArrayList<String> networks=new ArrayList<String>();
+        final ArrayList<String> networks=new ArrayList<String>();
         // TODO: needs a take photo intent for when where is no camera
 
         if (extras != null) {
@@ -93,7 +104,7 @@ final ArrayList<String> networks=new ArrayList<String>();
     public void OnFinalize(View view) {
         try {
             // Save user with the updated input in Profile model
-            userProfile = new Profile();
+
             userProfile.setFullName(txtName.getText().toString());
             if(spnNetwork.getSelectedItem().toString().equals("select")){
                 userProfile.setNetwork("");
@@ -104,7 +115,6 @@ final ArrayList<String> networks=new ArrayList<String>();
 
             userProfile.setJobTitle(txtjobTitle.getText().toString());
             userProfile.setUser(ParseUser.getCurrentUser());
-//            userProfile.setImageUrl(filepath);
             userProfile.save();
             Intent i = new Intent(UserProfileActivity.this, TimelineActivity.class);
             startActivity(i);
@@ -121,6 +131,7 @@ final ArrayList<String> networks=new ArrayList<String>();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap finalImg=null;
         try {
             // When an Image is picked
             if (requestCode == SELECTED_PICTURE && resultCode == RESULT_OK
@@ -128,7 +139,7 @@ final ArrayList<String> networks=new ArrayList<String>();
 
                 // Get the Image from data
                 Uri selectedImage = data.getData();
-                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
                 // Get the cursor
                 Cursor cursor = getContentResolver().query(selectedImage,
@@ -138,21 +149,112 @@ final ArrayList<String> networks=new ArrayList<String>();
                 cursor.moveToFirst();
 
                 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                 filepath = cursor.getString(columnIndex);
+                filepath = cursor.getString(columnIndex);
                 cursor.close();
+                Bitmap bmap=BitmapFactory
+                        .decodeFile(filepath);
+               finalImg = Bitmap.createScaledBitmap(bmap, 200, 200, true);
                 ImageView imgView = (ImageView) findViewById(R.id.ivphoto);
                 // Set the Image in ImageView after decoding the String
-                imgView.setImageBitmap(BitmapFactory
-                        .decodeFile(filepath));
+                imgView.setImageBitmap(finalImg);
+            }
 
-            } else {
-                Toast.makeText(this, "You haven't picked Image",
-                        Toast.LENGTH_LONG).show();
+            if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+                if (resultCode == RESULT_OK) {
+                    Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+
+                    // by this point we have the camera photo on disk
+                    //Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                    Bitmap rotateImg = rotateBitmapOrientation(takenPhotoUri.getPath());
+
+                    finalImg = Bitmap.createScaledBitmap(rotateImg, 200, 200, true);
+                    // Load the taken image into a preview
+                    ImageView ivPreview = (ImageView) findViewById(R.id.ivphoto);
+                    ivPreview.setImageBitmap(finalImg );
+                } else { // Result was a failure
+                    Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            if(finalImg !=null) {
+                finalImg.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                // get byte array here
+                byte[] bytearray = stream.toByteArray();
+                ParseFile imgFile = new ParseFile("profileImg.png", bytearray);
+                imgFile.saveInBackground();
+                userProfile.setPhotoFile(imgFile);
             }
         } catch (Exception e) {
             Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
                     .show();
         }
 
+    }
+
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
+        // Start the image capture intent to take photo
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            File mediaStorageDir = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        }
+        return null;
+    }
+
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Bitmap rotateBitmapOrientation(String photoFilePath) {
+        // Create and configure BitmapFactory
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFilePath, bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
+        // Read EXIF Data
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(photoFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        // Rotate Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        // Return result
+        return rotatedBitmap;
     }
 }
