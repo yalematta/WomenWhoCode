5a79ae726f835142ce35f429a27a7f8ec95e8355
Shehba Shahab <shehbashahab@gmail.com>
2015-10-30 14:21:52 -0700

    Updated TimelineAdapter to use View Holder pattern

--- app/src/main/java/com/example/womenwhocode/womenwhocode/adapters/TimelineAdapter.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/adapters/TimelineAdapter.java
@@ -27,19 +27,11 @@ import java.util.List;
  * Created by shehba.shahab on 10/17/15.
  */
 public class TimelineAdapter extends ArrayAdapter<Post> {
-
+    private ViewHolder viewHolder;
     private ParseUser currentUser;
     private Post post;
     private Awesome awesome;
     private int awesomeCount;
-    private ImageView ivFeaturePhoto;
-    private TextView tvPostDescription;
-    private TextView tvAwesomeCount;
-    private TextView tvAwesomeIcon;
-    private ProgressBar pb;
-    private LinearLayout llTimelineItems;
-    private TextView tvRelativeDate;
-    private TextView tvFeatureTitle;
 
     public TimelineAdapter(Context context, List<Post> objects) {
         super(context, android.R.layout.simple_list_item_1, objects);
@@ -49,28 +41,35 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
     public View getView(int position, View convertView, ViewGroup parent) {
         post = getItem(position);
         currentUser = ParseUser.getCurrentUser();
-        // Check if we are using a recycled view, if not we need to inflate
+
         if (convertView == null) {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_timeline, parent, false);
+            viewHolder.pb = (ProgressBar) convertView.findViewById(R.id.pbLoading);
+
+            // Hide scroll view so the progress bar is the center of attention
+            viewHolder.llTimelineItems = (LinearLayout) convertView.findViewById(R.id.llTimelineItems);
+
+            // Look up views to populate data
+            viewHolder.ivFeaturePhoto = (ImageView) convertView.findViewById(R.id.ivPostPhoto);
+            viewHolder.tvPostDescription = (TextView) convertView.findViewById(R.id.tvPostDescription);
+            viewHolder.tvAwesomeCount = (TextView) convertView.findViewById(R.id.tvAwesomeCount);
+            viewHolder.tvAwesomeIcon = (TextView) convertView.findViewById(R.id.tvAwesomeIcon);
+            viewHolder.tvRelativeDate = (TextView) convertView.findViewById(R.id.tvRelativeDate);
+            viewHolder.tvFeatureTitle = (TextView) convertView.findViewById(R.id.tvPostTitle);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        // set the progress bar
-        pb = (ProgressBar) convertView.findViewById(R.id.pbLoading);
-        pb.setVisibility(ProgressBar.VISIBLE);
+        // Set the progress bar
+        viewHolder.pb.setVisibility(ProgressBar.VISIBLE);
 
-        // hide scroll view so the progress bar is the center of attention
-        llTimelineItems = (LinearLayout) convertView.findViewById(R.id.llTimelineItems);
-        llTimelineItems.setVisibility(ScrollView.INVISIBLE);
+        // Hide scroll view so the progress bar is the center of attention
+        viewHolder.llTimelineItems = (LinearLayout) convertView.findViewById(R.id.llTimelineItems);
+        viewHolder.llTimelineItems.setVisibility(ScrollView.INVISIBLE);
 
-        // Look up views to populate data
-        ivFeaturePhoto = (ImageView) convertView.findViewById(R.id.ivPostPhoto);
-        tvPostDescription = (TextView) convertView.findViewById(R.id.tvPostDescription);
-        tvAwesomeCount = (TextView) convertView.findViewById(R.id.tvAwesomeCount);
-        tvAwesomeIcon = (TextView) convertView.findViewById(R.id.tvAwesomeIcon);
-        tvRelativeDate = (TextView) convertView.findViewById(R.id.tvRelativeDate);
-        tvFeatureTitle = (TextView) convertView.findViewById(R.id.tvPostTitle);
-
-        tvAwesomeIcon.setOnClickListener(new View.OnClickListener() {
+        viewHolder.tvAwesomeIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onAwesome();
@@ -88,25 +87,27 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
                 } else {
                     awesomeCount = 0;
                 }
+
                 // Clear out the image views
-                ivFeaturePhoto.setImageResource(0);
+                viewHolder.ivFeaturePhoto.setImageResource(0);
 
                 // Insert the model data into each of the view items
                 String description = post.getDescription();
-                tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
                 String relativeDate = post.getPostDateTime();
                 String featureImageUrl = post.getFeatureImageUrl();
                 String featureTitle = post.getFeatureTitle();
-                tvPostDescription.setText(description);
-                tvRelativeDate.setText(relativeDate);
-                tvFeatureTitle.setText(featureTitle);
+
+                viewHolder.tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
+                viewHolder.tvPostDescription.setText(description);
+                viewHolder.tvRelativeDate.setText(relativeDate);
+                viewHolder.tvFeatureTitle.setText(featureTitle);
 
                 // Insert the image using picasso
-                Picasso.with(getContext()).load(featureImageUrl).into(ivFeaturePhoto);
+                Picasso.with(getContext()).load(featureImageUrl).into(viewHolder.ivFeaturePhoto);
 
-                // hide the progress bar, show the main view
-                pb.setVisibility(ProgressBar.GONE);
-                llTimelineItems.setVisibility(ScrollView.VISIBLE);
+                // Hide the progress bar, show the main view
+                viewHolder.pb.setVisibility(ProgressBar.GONE);
+                viewHolder.llTimelineItems.setVisibility(ScrollView.VISIBLE);
             }
         });
 
@@ -119,7 +120,7 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
 
                 // Update UI thread
                 awesomeCount--;
-                tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
+                viewHolder.tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
 
                 // Send data to Parse
                 awesome.setAwesomed(false);
@@ -132,7 +133,7 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
             } else {
                 // Update UI thread
                 awesomeCount++;
-                tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
+                viewHolder.tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
 
                 // Send data to Parse
                 awesome.setAwesomed(true);
@@ -146,7 +147,7 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
         } else {
             // Update UI thread
             awesomeCount++;
-            tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
+            viewHolder.tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
 
             // Send data to Parse
             awesome = new Awesome();
@@ -160,5 +161,16 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
                 }
             });
         }
+    }
+
+    private static class ViewHolder {
+        ImageView ivFeaturePhoto;
+        TextView tvPostDescription;
+        TextView tvAwesomeCount;
+        TextView tvAwesomeIcon;
+        LinearLayout llTimelineItems;
+        TextView tvRelativeDate;
+        TextView tvFeatureTitle;
+        ProgressBar pb;
     }
 }
