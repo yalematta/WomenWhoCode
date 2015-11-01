a88949d707dcba3c9b085b15e2d91f3215e1e5f9
Shehba Shahab <shehbashahab@gmail.com>
2015-10-31 17:35:01 -0700

    Another partial update for awesomeCounts on Posts

--- app/src/main/java/com/example/womenwhocode/womenwhocode/adapters/PostsAdapter.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/adapters/PostsAdapter.java
@@ -11,9 +11,7 @@ import android.widget.TextView;
 import com.example.womenwhocode.womenwhocode.R;
 import com.example.womenwhocode.womenwhocode.models.Awesome;
 import com.example.womenwhocode.womenwhocode.models.Post;
-import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -24,19 +22,11 @@ import java.util.ArrayList;
 public class PostsAdapter extends ArrayAdapter<Post> {
 
     Post post;
+    ViewHolder viewHolder;
     private ParseUser currentUser;
     private Awesome awesome;
     private int awesomeCount;
     private ParseUser postUser;
-
-    private static class ViewHolder {
-        ImageView ivUserPhoto;
-        TextView tvPostNameBy;
-        TextView tvPostDescription;
-        TextView tvAwesomeCount;
-        TextView tvAwesomeIcon;
-        TextView tvRelativeTime;
-    }
 
     public PostsAdapter(Context context, ArrayList<Post> posts) {
         super(context, android.R.layout.simple_list_item_1, posts);
@@ -46,24 +36,25 @@ public class PostsAdapter extends ArrayAdapter<Post> {
     public View getView(int position, View convertView, ViewGroup parent) {
         post = getItem(position);
         currentUser = ParseUser.getCurrentUser();
-
-        final ViewHolder viewHolder;
         if (convertView == null) {
-            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(
                     R.layout.item_post, parent, false);
+
+            viewHolder = new ViewHolder();
             viewHolder.ivUserPhoto = (ImageView) convertView.findViewById(R.id.ivUserPhoto);
             viewHolder.tvPostNameBy = (TextView) convertView.findViewById(R.id.tvPostNameBy);
             viewHolder.tvPostDescription = (TextView) convertView.findViewById(R.id.tvPostDescription);
             viewHolder.tvAwesomeCount = (TextView) convertView.findViewById(R.id.tvAwesomeCount);
             viewHolder.tvAwesomeIcon = (TextView) convertView.findViewById(R.id.tvAwesomeIcon);
             viewHolder.tvRelativeTime = (TextView) convertView.findViewById(R.id.tvRelativeTime);
+
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
         postUser = post.getUser();
+
         if (postUser != null) {
             viewHolder.tvPostNameBy.setText(postUser.getUsername());
             // TODO: find the profile for user to get their image :(
@@ -75,70 +66,63 @@ public class PostsAdapter extends ArrayAdapter<Post> {
 
         viewHolder.tvPostDescription.setText(post.getDescription());
 
-        int awesomeCount = Awesome.getAwesomeCountFor(post);
-        viewHolder.tvAwesomeCount.setText(Integer.toString(awesomeCount));
+        int awesomeCount = post.getAwesomeCount();
+        viewHolder.tvAwesomeCount.setText(String.valueOf(awesomeCount));
 
-        // TODO: Hack - will consolidate into single icon for awesome/unawesome
         viewHolder.tvAwesomeIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onAwesome(viewHolder.tvAwesomeCount);
+                onAwesome();
             }
         });
-
-        // TODO: load awesome icon
 
         viewHolder.tvRelativeTime.setText(post.getPostDateTime());
 
         return convertView;
     }
 
-    private void onAwesome(TextView tvAwesomeCount) {
+    private void onAwesome() {
+        awesomeCount = post.getAwesomeCount(); // Get latest value
         if (awesome != null) {
             if (awesome.getAwesomed()) {
-
                 // Update UI thread
                 awesomeCount--;
-                tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
 
-                // Send data to Parse
+                // Build parse request
                 awesome.setAwesomed(false);
-                awesome.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        // TODO: Sync Parse response with UI thread
-                    }
-                });
             } else {
                 // Update UI thread
                 awesomeCount++;
-                tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
 
-                // Send data to Parse
+                // Build parse request
                 awesome.setAwesomed(true);
-                awesome.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        // TODO: Sync Parse response with UI thread
-                    }
-                });
             }
         } else {
             // Update UI thread
             awesomeCount++;
-            tvAwesomeCount.setText(Integer.valueOf(awesomeCount).toString());
 
-            // Send data to Parse
+            // Build parse request
             awesome = new Awesome();
             awesome.setAwesomed(true);
             awesome.setUser(currentUser);
             awesome.setPost(post);
-            awesome.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    // TODO: Sync Parse response with UI thread
-                }
-            });
         }
+
+        // Update the UI thread
+        viewHolder.tvAwesomeCount.setText(String.valueOf(awesomeCount));
+
+        // Send data to parse
+        awesome.saveInBackground();
+        post.setAwesomeCount(awesomeCount);
+        post.saveInBackground();
+    }
+
+    private static class ViewHolder {
+        ImageView ivUserPhoto;
+        TextView tvPostNameBy;
+        TextView tvPostDescription;
+        TextView tvAwesomeCount;
+        TextView tvAwesomeIcon;
+        TextView tvRelativeTime;
     }
 }
--- app/src/main/java/com/example/womenwhocode/womenwhocode/models/Awesome.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/models/Awesome.java
@@ -1,11 +1,9 @@
 package com.example.womenwhocode.womenwhocode.models;
 
 import com.parse.ParseClassName;
-import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 /**
  * Created by shehba.shahab on 10/24/15.
@@ -21,31 +19,6 @@ public class Awesome extends ParseObject {
 
     public static ParseQuery<Awesome> getQuery() {
         return ParseQuery.getQuery(Awesome.class);
-    }
-
-    public static boolean isAwesomeForUser(ParseUser user, Post post) {
-        query = Awesome.getQuery();
-        query.whereEqualTo(USER_KEY, user);
-        query.whereEqualTo(POST_KEY, post);
-        Integer count = query.countInBackground().getResult();
-        return count != null;
-    }
-
-    // Make this work async
-    public static int getAwesomeCountFor(Post post) {
-        query = Awesome.getQuery();
-        query.whereEqualTo(POST_KEY, post);
-        //Integer count = query.countInBackground().getResult();
-        Integer count = null;
-        try {
-            count = query.count();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        if (count == null) {
-            return 0;
-        }
-        return count;
     }
 
     public boolean getAwesomed() {
@@ -72,29 +45,4 @@ public class Awesome extends ParseObject {
         put(USER_KEY, user);
     }
 
-    public static void awesomePostForCurrentUser(final Post post) {
-        final ParseObject awesome = new ParseObject("Awesome");
-        final ParseUser user = ParseUser.getCurrentUser();
-        awesome.saveInBackground(new SaveCallback() {
-            public void done(ParseException e) {
-                awesome.put(USER_KEY, user);
-                awesome.put(POST_KEY, post);
-                awesome.put(AWESOMED_KEY, true);
-                awesome.saveInBackground();
-            }
-        });
-    }
-
-    public static void unAwesomePostForCurrentUser(final Post post) {
-        final ParseObject awesome = new ParseObject("Awesome");
-        final ParseUser user = ParseUser.getCurrentUser();
-        awesome.saveInBackground(new SaveCallback() {
-            public void done(ParseException e) {
-                awesome.put(USER_KEY, user);
-                awesome.put(POST_KEY, post);
-                awesome.put(AWESOMED_KEY, false);
-                awesome.saveInBackground();
-            }
-        });
-    }
 }
