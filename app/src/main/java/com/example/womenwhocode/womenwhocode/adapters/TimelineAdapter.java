9850195daf77e7cdf5e794bfc233944841436140
zassmin <zmontesd@gmail.com>
2015-11-08 15:25:44 -0800

    implement recycler view for TimelineFragment and Adapter and transition listeners

--- app/src/main/java/com/example/womenwhocode/womenwhocode/adapters/TimelineAdapter.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/adapters/TimelineAdapter.java
@@ -1,16 +1,12 @@
 package com.example.womenwhocode.womenwhocode.adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationUtils;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -18,17 +14,10 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.womenwhocode.womenwhocode.R;
-import com.example.womenwhocode.womenwhocode.activities.EventDetailsActivity;
-import com.example.womenwhocode.womenwhocode.activities.FeatureDetailsActivity;
-import com.example.womenwhocode.womenwhocode.activities.TimelineActivity;
-import com.example.womenwhocode.womenwhocode.models.Awesome;
 import com.example.womenwhocode.womenwhocode.models.Event;
 import com.example.womenwhocode.womenwhocode.models.Feature;
 import com.example.womenwhocode.womenwhocode.models.Post;
 import com.example.womenwhocode.womenwhocode.utils.CircleTransform;
-import com.parse.GetCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
 
@@ -40,60 +29,62 @@ import butterknife.ButterKnife;
 /**
  * Created by shehba.shahab on 10/17/15.
  */
-public class TimelineAdapter extends ArrayAdapter<Post> {
-    private ParseUser currentUser;
+public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ViewHolder> {
+    private List<Post> mPosts;
+    private static OnItemClickListener listener;
 
-    public TimelineAdapter(Context context, List<Post> objects) {
-        super(context, android.R.layout.simple_list_item_1, objects);
+    public interface OnItemClickListener {
+        void onPostFeatureClick(View itemView, int position);
+        void onAwesomeClick(View itemView, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public TimelineAdapter(List<Post> posts) {
+        mPosts = posts;
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        Post post = getItem(position);
-        currentUser = ParseUser.getCurrentUser();
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View timelineView = inflater.inflate(R.layout.item_timeline, parent, false);
+        ViewHolder viewHolder = new ViewHolder(timelineView);
+        return viewHolder;
+    }
 
-        final ViewHolder holder;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_timeline, parent, false);
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Post post = mPosts.get(position);
 
-            holder = new ViewHolder(convertView);
-            holder.post = post;
-
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        // TODO: set awesome icon here - default - not awesome yet
-        // get the awesome object
-        ParseQuery<Awesome> awesomeParseQuery = ParseQuery.getQuery(Awesome.class);
-        awesomeParseQuery.whereEqualTo(Awesome.POST_KEY, post);
-        awesomeParseQuery.whereEqualTo(Awesome.USER_KEY, currentUser);
-        awesomeParseQuery.getFirstInBackground(new GetCallback<Awesome>() {
-            @Override
-            public void done(Awesome a, ParseException e) {
-                if (e == null) {
-                    holder.tvAwesomeIcon.setTag(a);
-                    // TODO if awesome.getAwesome == true change the icon to the awesomedd icon
-                } else {
-                    holder.tvAwesomeIcon.setTag(null);
-                }
-            }
-        });
+        ProgressBar pb = holder.pb;
+        CardView cvPostFeature = holder.cvPostFeature;
+        ImageView ivPostPhoto = holder.ivPostPhoto;
+        RelativeLayout rlPostFeature = holder.rlPostFeature;
+        TextView tvPostNameBy = holder.tvPostNameBy;
+        TextView tvPostDescription = holder.tvPostDescription;
+        TextView tvRelativeDate = holder.tvRelativeDate;
+        TextView tvFeatureTitle = holder.tvFeatureTitle;
+        TextView tvAwesomeCount = holder.tvAwesomeCount;
+        ImageButton btnAwesomeIcon = holder.btnAwesomeIcon;
 
         // Set the progress bar
-        holder.pb.setVisibility(ProgressBar.VISIBLE);
-
+        pb.setVisibility(ProgressBar.VISIBLE);
         // Hide relative layout so the progress bar is the center of attention
-        holder.cvPostFeature.setVisibility(CardView.INVISIBLE);
+        cvPostFeature.setVisibility(CardView.INVISIBLE);
 
-        // Clear out the image views
-        holder.ivPostPhoto.setImageResource(0);
+        // to resolve bug where icon changes on many items
+        btnAwesomeIcon.setImageResource(0);
+        btnAwesomeIcon.setImageResource(R.drawable.awesome);
 
+        // Set up feature/event specific attrs
+        ivPostPhoto.setImageResource(0);
+        Context context = ivPostPhoto.getContext();
         String title = "WWCode";
-        // final so on click of feature has access to it, these values don't change anyway
-        final Feature feature = post.getFeature();
-        final Event event = post.getEvent();
+        Feature feature = post.getFeature();
+        Event event = post.getEvent();
         if (feature != null) {
             String imageUrl = feature.getImageUrl();
             title = feature.getTitle();
@@ -102,27 +93,27 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
             // set feature background color
             // set color!
             int color = Color.parseColor(String.valueOf(hexColor));
-            holder.rlPostFeature.setBackgroundColor(color); // default color is set in xml
+            rlPostFeature.setBackgroundColor(color); // default color is set in xml
 
             // Insert the image using picasso
-            Picasso.with(getContext())
+            Picasso.with(context)
                     .load(imageUrl)
                     .transform(new CircleTransform())
                     .resize(75, 75)
                     .centerCrop()
-                    .into(holder.ivPostPhoto);
+                    .into(ivPostPhoto);
 
         } else if (event != null) {
             title = event.getTitle();
 
             // insert icon
-            holder.ivPostPhoto.setImageResource(R.drawable.ic_calendar_check);
+            ivPostPhoto.setImageResource(R.drawable.ic_calendar_check); // since we know at this point they are following
         }
 
         // in case a post has a user
         ParseUser postUser = post.getUser();
-        if (postUser != null) {
-            holder.tvPostNameBy.setText(postUser.getUsername());
+        if (postUser != null) { // FIXME: should be full name from profile
+            tvPostNameBy.setText(postUser.getUsername());
         }
 
         // Insert the model data into each of the view items
@@ -130,146 +121,56 @@ public class TimelineAdapter extends ArrayAdapter<Post> {
         String relativeDate = post.getPostDateTime();
         int awesomeCount = post.getAwesomeCount();
 
-        holder.tvPostDescription.setText(description);
-        holder.tvRelativeDate.setText(relativeDate);
-        holder.tvFeatureTitle.setText(title);
-        holder.tvAwesomeCount.setText(String.valueOf(awesomeCount));
-
-        // Store all necessary data for click
-        holder.tvPostDescription.setTag(post);
+        tvPostDescription.setText(description);
+        tvRelativeDate.setText(relativeDate);
+        tvFeatureTitle.setText(title);
+        tvAwesomeCount.setText(String.valueOf(awesomeCount));
 
         // Hide the progress bar, show the main view
-        holder.pb.setVisibility(ProgressBar.GONE);
-        holder.cvPostFeature.setVisibility(CardView.VISIBLE);
+        pb.setVisibility(ProgressBar.GONE);
+        cvPostFeature.setVisibility(CardView.VISIBLE);
+    }
 
-        holder.tvAwesomeIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                View parent = (View) v.getParent();
-                // grab the tagged objects
-                Post post = (Post) parent.findViewById(R.id.tvPostDescription).getTag();
-                Awesome awesome = (Awesome) v.getTag();
-                TextView tvAwesomeCount = (TextView) parent.findViewById(R.id.tvAwesomeCount);
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
 
-                // TODO: start icon and counter animation here!
-                // check which icon is it - awesome or unawesome
-                // do animation on the icon
-                // switch them with a nice scale in out
-                // update count value based on awesome count (+||-)
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.ivPostPhoto) public ImageView ivPostPhoto;
+        @Bind(R.id.tvPostDescription) public TextView tvPostDescription;
+        @Bind(R.id.tvAwesomeCount) public TextView tvAwesomeCount;
+        @Bind(R.id.btnAwesomeIcon) public ImageButton btnAwesomeIcon;
+        @Bind(R.id.cvPostFeature) public CardView cvPostFeature;
+        @Bind(R.id.tvRelativeDate) public TextView tvRelativeDate;
+        @Bind(R.id.tvPostTitle) public TextView tvFeatureTitle;
+        @Bind(R.id.pbLoading) public ProgressBar pb;
+        @Bind(R.id.rlPostFeature) public RelativeLayout rlPostFeature;
+        @Bind(R.id.tvPostNameBy) public TextView tvPostNameBy;
 
-                // could the value of a be null?
-                onAwesome(tvAwesomeCount, awesome, post, v);
-            }
-        });
+        public ViewHolder(final View itemView) {
+            super(itemView);
 
-        holder.rlPostFeature.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (feature != null) {
-                    Intent i = new Intent(getContext(), FeatureDetailsActivity.class);
-                    i.putExtra("feature_id", feature.getObjectId());
-                    i.putExtra(TimelineActivity.SELECTED_TAB_EXTRA_KEY, TimelineActivity.TIMELINE_TAB);
-                    getContext().startActivity(i);
-                } else if (event != null) {
-                    Intent i = new Intent(getContext(), EventDetailsActivity.class);
-                    i.putExtra("event_id", event.getObjectId());
-                    i.putExtra(TimelineActivity.SELECTED_TAB_EXTRA_KEY, TimelineActivity.TIMELINE_TAB);
-                    getContext().startActivity(i);
+            ButterKnife.bind(this, itemView);
+
+            // FIXME: this may not work and I may need to reference: http://stackoverflow.com/questions/24885223/why-doesnt-recyclerview-have-onitemclicklistener-and-how-recyclerview-is-dif/24933117#24933117
+            rlPostFeature.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onPostFeatureClick(itemView, getLayoutPosition());
+                    }
                 }
-            }
-        });
-        return convertView;
-    }
+            });
 
-    private void animateOnAwesome(final ImageButton awesomeIcon) {
-        Animation animateOnAwesome;
-        animateOnAwesome = AnimationUtils.loadAnimation(getContext(),
-                R.anim.scale_up);
-        awesomeIcon.startAnimation(animateOnAwesome);
-        animateOnAwesome.setAnimationListener(new AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                awesomeIcon.setImageResource(R.drawable.awesomeddd);
-            }
-
-            public void onAnimationEnd(Animation anim) {
-                awesomeIcon.setImageResource(R.drawable.awesome);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-    }
-
-    private void animateOnUnawesome(ImageButton awesomeIcon) {
-        Animation animation;
-        animation = AnimationUtils.loadAnimation(getContext(),
-                R.anim.scale_down);
-        awesomeIcon.startAnimation(animation);
-    }
-
-
-    private void onAwesome(TextView tvAwesomeCount, Awesome awesome, Post savedPost, View v) {
-        int awesomeCount = savedPost.getAwesomeCount(); // Get latest value
-        ImageButton awesomeIcon = (ImageButton) v.findViewById(R.id.btnAwesomeIcon);
-
-        if (awesome != null) {
-            if (awesome.getAwesomed()) {
-                // Update UI thread
-                awesomeCount--;
-                animateOnUnawesome(awesomeIcon);
-
-                // Build parse request
-                awesome.setAwesomed(false);
-            } else {
-                // Update UI thread
-                awesomeCount++;
-                animateOnAwesome(awesomeIcon);
-
-                // Build parse request
-                awesome.setAwesomed(true);
-            }
-        } else {
-            // Update UI thread
-            awesomeCount++;
-            animateOnAwesome(awesomeIcon);
-
-            // Build parse request
-            awesome = new Awesome();
-            awesome.setAwesomed(true);
-            awesome.setUser(currentUser);
-            awesome.setPost(savedPost);
-        }
-
-        // Update the UI thread
-        // TODO: it's probably safe to do this before the onAwesome
-        tvAwesomeCount.setText(String.valueOf(awesomeCount));
-        // reset the awesome account in case it was null before!
-        v.setTag(awesome);
-
-        // Send data to parse
-        awesome.saveInBackground();
-        savedPost.setAwesomeCount(awesomeCount);
-        savedPost.saveInBackground();
-    }
-
-    static class ViewHolder {
-        @Bind(R.id.ivPostPhoto) ImageView ivPostPhoto;
-        @Bind(R.id.tvPostDescription) TextView tvPostDescription;
-        @Bind(R.id.tvAwesomeCount) TextView tvAwesomeCount;
-        @Bind(R.id.btnAwesomeIcon) ImageButton tvAwesomeIcon;
-        @Bind(R.id.cvPostFeature) CardView cvPostFeature;
-        @Bind(R.id.tvRelativeDate) TextView tvRelativeDate;
-        @Bind(R.id.tvPostTitle) TextView tvFeatureTitle;
-        @Bind(R.id.pbLoading) ProgressBar pb;
-        @Bind(R.id.rlPostFeature) RelativeLayout rlPostFeature;
-        @Bind(R.id.tvPostNameBy) TextView tvPostNameBy;
-        Post post;
-
-        ViewHolder(View view) {
-            ButterKnife.bind(this, view);
+            btnAwesomeIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onAwesomeClick(itemView, getLayoutPosition());
+                    }
+                }
+            });
         }
     }
 }
