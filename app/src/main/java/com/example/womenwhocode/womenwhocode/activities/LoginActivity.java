14a1ba1461171d29e132a8261c4abc3a97fe5337
zassmin <zmontesd@gmail.com>
2015-11-12 01:15:04 -0800

    make landing page gif replayable

--- app/src/main/java/com/example/womenwhocode/womenwhocode/activities/LoginActivity.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/activities/LoginActivity.java
@@ -2,14 +2,22 @@ package com.example.womenwhocode.womenwhocode.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.womenwhocode.womenwhocode.R;
 
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+import pl.droidsonroids.gif.MultiCallback;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
@@ -19,6 +27,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // give gif an endless loop!
+        GifImageView gifImageView = (GifImageView) findViewById(R.id.givIntro);
+        GifDrawable gifDrawable = (GifDrawable) gifImageView.getDrawable();
+        gifDrawable.setLoopCount(0); // not too helpful
     }
 
     @Override
