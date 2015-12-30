007506a01761e05bb9be4f5cbb6cb6448cba1b63
chhavip <coolaqua12@gmail.com>
2015-12-30 14:43:21 +0530

    Google Client Crash Fix

--- app/src/main/java/com/example/womenwhocode/womenwhocode/utils/LocationProvider.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/utils/LocationProvider.java
@@ -102,9 +102,11 @@ public class LocationProvider implements
     }
 
     public void disconnect() {
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            mGoogleApiClient.disconnect();
+        if(mGoogleApiClient!=null) {
+            if (mGoogleApiClient.isConnected()) {
+                LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+                mGoogleApiClient.disconnect();
+            }
         }
     }
 
