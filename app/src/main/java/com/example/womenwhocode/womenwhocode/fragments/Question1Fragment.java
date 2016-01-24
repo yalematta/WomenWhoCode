c1c6a20149294cee748d0e85d8f365c73e4fdaad
Shehba Shahab <shehbashahab@gmail.com>
2016-01-23 19:37:18 -0800

    Reformatted code and optimized imports

--- app/src/main/java/com/example/womenwhocode/womenwhocode/fragments/Question1Fragment.java
+++ app/src/main/java/com/example/womenwhocode/womenwhocode/fragments/Question1Fragment.java
@@ -1,6 +1,5 @@
 package com.example.womenwhocode.womenwhocode.fragments;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
